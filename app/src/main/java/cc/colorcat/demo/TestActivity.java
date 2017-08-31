cd174dadc593a0c004c36a67ead4056073fd4a49
cxx <xx.ch@outlook.com>
2017-08-31 14:14:18 +0800

    add TernaryTextView

--- app/src/main/java/cc/colorcat/demo/TestActivity.java
+++ app/src/main/java/cc/colorcat/demo/TestActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.text.TextPaint;
 import android.view.View;
-import android.widget.TextView;
 
 import java.util.Random;
 
@@ -22,22 +21,15 @@ public class TestActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_test);
-
-        TextView textView = (TextView) findViewById(R.id.text_view);
-//        textView.setTextSize(12);
-//        textView.getTextSize();
-//        textView.setTextColor();
-//        textView.setText("test");
+        setContentView(R.layout.activity_test);
 
         final TernaryTextView ttv = (TernaryTextView) findViewById(R.id.ttv);
-//        ttv.setText("01234");
         ttv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                ttv.setTextSize(ttv.getTextSize() + 5);
-//                ttv.setTextColor(Color.argb(255, RANDOM.nextInt(256), RANDOM.nextInt(256), RANDOM.nextInt(256)));
-//                ttv.setText(Integer.toString(RANDOM.nextInt()));
+                ttv.setTextSize(ttv.getTextSize() + 2);
+                ttv.setTextColor(Color.argb(255, RANDOM.nextInt(256), RANDOM.nextInt(256), RANDOM.nextInt(256)));
+                ttv.setText(Integer.toString(RANDOM.nextInt()));
                 ttv.addPaintFlag(TextPaint.FAKE_BOLD_TEXT_FLAG);
             }
         });
