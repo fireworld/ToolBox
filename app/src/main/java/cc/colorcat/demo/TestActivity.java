6ed54fc86754db950364c389935277c2689828fe
cxx <xx.ch@outlook.com>
2017-08-31 11:51:06 +0800

    add setPadding()

--- app/src/main/java/cc/colorcat/demo/TestActivity.java
+++ app/src/main/java/cc/colorcat/demo/TestActivity.java
@@ -1,15 +1,23 @@
 package cc.colorcat.demo;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.TextPaint;
+import android.view.View;
 import android.widget.TextView;
+
+import java.util.Random;
+
+import cc.colorcat.toolbox.widget.TernaryTextView;
 
 /**
  * Created by cxx on 2017/8/30.
  * xx.ch@outlook.com
  */
 public class TestActivity extends Activity {
+    private static final Random RANDOM = new Random();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -17,6 +25,21 @@ public class TestActivity extends Activity {
         setContentView(R.layout.layout_test);
 
         TextView textView = (TextView) findViewById(R.id.text_view);
-//        textView.setText("fsdf");
+        textView.setTextSize(12);
+        textView.getTextSize();
+//        textView.setTextColor();
+        textView.setText("test");
+
+        final TernaryTextView ttv = (TernaryTextView) findViewById(R.id.ttv);
+//        ttv.setText("01234");
+        ttv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                ttv.setTextSize(ttv.getTextSize() + 5);
+//                ttv.setTextColor(Color.argb(255, RANDOM.nextInt(256), RANDOM.nextInt(256), RANDOM.nextInt(256)));
+//                ttv.setText(Integer.toString(RANDOM.nextInt()));
+                ttv.addPaintFlag(TextPaint.FAKE_BOLD_TEXT_FLAG);
+            }
+        });
     }
 }
--- toolbox/src/main/java/cc/colorcat/toolbox/widget/TernaryTextView.java
+++ toolbox/src/main/java/cc/colorcat/toolbox/widget/TernaryTextView.java
@@ -1,6 +1,7 @@
 package cc.colorcat.toolbox.widget;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -28,9 +29,6 @@ public class TernaryTextView extends View {
     private Drawable[] mDrawables = new Drawable[3];
     private Rect[] mBounds = {new Rect(), new Rect(), new Rect()};
     private CharSequence mText;
-    private int mTextSize;
-    @ColorInt
-    private int mTextColor;
     private Paint mTextPaint;
 
     public TernaryTextView(Context context) {
@@ -60,13 +58,102 @@ public class TernaryTextView extends View {
         mDrawables[1] = ta.getDrawable(R.styleable.TernaryTextView_drawableCenter);
         mDrawables[2] = ta.getDrawable(R.styleable.TernaryTextView_drawableEnd);
         mText = ta.getText(R.styleable.TernaryTextView_text);
-        mTextSize = ta.getDimensionPixelSize(R.styleable.TernaryTextView_textSize, defaultSize);
-        mTextColor = ta.getColor(R.styleable.TernaryTextView_textColor, Color.DKGRAY);
+        if (mText == null) mText = "";
+        int textSize = ta.getDimensionPixelSize(R.styleable.TernaryTextView_textSize, defaultSize);
+        int textColor = ta.getColor(R.styleable.TernaryTextView_textColor, Color.BLACK);
         ta.recycle();
         mTextPaint = new Paint();
         mTextPaint.setAntiAlias(true);
-        mTextPaint.setTextSize(mTextSize);
-        mTextPaint.setColor(mTextColor);
+        mTextPaint.setTextSize(textSize);
+        mTextPaint.setColor(textColor);
+    }
+
+    public void setText(CharSequence text) {
+        CharSequence cs = (text != null ? text : "");
+        if (!cs.equals(mText)) {
+            mText = cs;
+            requestLayout();
+            invalidate();
+        }
+    }
+
+    public void setTextColor(@ColorInt int color) {
+        if (color != mTextPaint.getColor()) {
+            mTextPaint.setColor(color);
+            invalidate();
+        }
+    }
+
+    public void setTextSize(float sizePx) {
+        setRawTextSize(sizePx);
+    }
+
+    /**
+     * Set the default text size to a given unit and value.  See {@link
+     * TypedValue} for the possible dimension units.
+     *
+     * @param unit The desired dimension unit.
+     * @param size The desired size in the given units.
+     */
+    public void setTextSize(int unit, float size) {
+        Context c = getContext();
+        Resources r;
+        if (c == null) {
+            r = Resources.getSystem();
+        } else {
+            r = c.getResources();
+        }
+        setRawTextSize(TypedValue.applyDimension(unit, size, r.getDisplayMetrics()));
+    }
+
+    public float getTextSize() {
+        return mTextPaint.getTextSize();
+    }
+
+    private void setRawTextSize(float size) {
+        if (size != mTextPaint.getTextSize()) {
+            mTextPaint.setTextSize(size);
+            requestLayout();
+            invalidate();
+        }
+    }
+
+    public void setPaintFlags(int flags) {
+        if (flags != mTextPaint.getFlags()) {
+            mTextPaint.setFlags(flags);
+            requestLayout();
+            invalidate();
+        }
+    }
+
+    public void addPaintFlag(int flag) {
+        int oldFlags = mTextPaint.getFlags();
+        int newFlags = oldFlags | flag;
+        if (newFlags != oldFlags) {
+            mTextPaint.setFlags(newFlags);
+            requestLayout();
+            invalidate();
+        }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        L.d("onMeasure");
+        Size reqP = computeRequireSize(getPaddingStart(), getPaddingTop(), getPaddingEnd(), getPaddingBottom());
+        setMeasuredDimension(measureSize(reqP.width, widthMeasureSpec), measureSize(reqP.height, heightMeasureSpec));
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        L.i(Utils.format("changed = %b, left = %d, top = %d, right = %d, bottom = %d",
+                changed, left, top, right, bottom));
+        if (changed) {
+            int width = right - left, height = bottom - top;
+            computeBounds(0, width, height);
+            computeBounds(2, width, height);
+            computeBounds(1, width, height);
+        }
     }
 
     @Override
@@ -84,81 +171,83 @@ public class TernaryTextView extends View {
         canvas.drawText(mText, 0, mText.length(), point.x, point.y, mTextPaint);
     }
 
-    @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        super.onLayout(changed, left, top, right, bottom);
-        L.i(Utils.format("changed = %b, left = %d, top = %d, right = %d, bottom = %d",
-                changed, left, top, right, bottom));
-        if (changed) {
-            int width = right - left, height = bottom - top;
-            computeBounds(0, width, height);
-            computeBounds(2, width, height);
-            computeBounds(1, width, height);
+    private int measureSize(int requireSize, int measureSpec) {
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+        switch (specMode) {
+            case MeasureSpec.EXACTLY:
+                return specSize;
+            case MeasureSpec.AT_MOST:
+                return Math.min(specSize, requireSize);
+            case MeasureSpec.UNSPECIFIED:
+                return requireSize;
+            default:
+                throw new IllegalArgumentException("wrong specMode = " + specMode);
         }
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        L.d("onMeasure");
     }
 
     private void computeBounds(int index, int viewWidth, int viewHeight) {
-        Drawable drawable = mDrawables[index];
-        mBounds[index].top = 0;
-        mBounds[index].bottom = viewHeight;
-        if (drawable != null) {
-            if (index != 1) {
-                int height = drawable.getMinimumHeight(), width = drawable.getMinimumWidth();
-                int drawWidth = height != 0 ? viewHeight * width / height : 0;
-                if (index == 0) {
-                    mBounds[0].left = 0;
-                    mBounds[0].right = mBounds[0].left + drawWidth;
-                } else if (index == 2) {
-                    mBounds[2].right = viewWidth;
-                    mBounds[2].left = mBounds[2].right - drawWidth;
-                }
-            } else {
-                mBounds[1].left = mBounds[0].right;
-                mBounds[1].right = mBounds[2].left;
-            }
+        Size size = computeDrawSize(viewHeight, mDrawables[index]);
+        Rect rect = mBounds[index];
+        rect.top = 0;
+        rect.bottom = size.height;
+        switch (index) {
+            case 0:
+                rect.left = 0;
+                rect.right = rect.left + size.width;
+                break;
+            case 1:
+                rect.left = mBounds[0].right;
+                rect.right = mBounds[2].left;
+                break;
+            case 2:
+                rect.right = viewWidth;
+                rect.left = rect.right - size.width;
+                break;
+            default:
+                break;
         }
     }
 
-    private int computeMinWidth() {
-        int center = mDrawables[1] != null ? mDrawables[1].getMinimumWidth() : 0;
-        center = (int) Math.max(center, mTextPaint.measureText(mText, 0, mText.length()));
-        return mBounds[0].width() + mBounds[2].width() + center;
+    private static Size computeDrawSize(int viewHeight, Drawable drawable) {
+        Size size = new Size();
+        // 获取 drawable 原始大小
+        if (drawable != null) {
+            size.width = drawable.getMinimumWidth();
+            size.height = drawable.getMinimumHeight();
+        }
+        // 将 drawable 的高缩放至 viewHeight 大小，同时drawable 的宽等比缩放
+        if (size.height != 0) {
+            size.width = viewHeight * size.width / size.height;
+        }
+        size.height = viewHeight;
+        return size;
     }
 
-    private int computeMinHeight() {
-        int height = 0;
+    private Size computeRequireSize(int paddingStart, int paddingTop, int paddingEnd, int paddingBottom) {
+        Size result = new Size();
+        Paint.FontMetrics fm = mTextPaint.getFontMetrics();
+        int textHeight = Math.round(0.5F + fm.bottom - fm.top);
+        int textWidth = Math.round(0.5F + mTextPaint.measureText(mText, 0, mText.length()));
         for (int size = mDrawables.length, index = 0; index < size; index++) {
             Drawable drawable = mDrawables[index];
             if (drawable != null) {
-                int tempHeight = drawable.getMinimumHeight();
-                if (tempHeight > height) {
-                    height = tempHeight;
+                int height = drawable.getMinimumHeight();
+                if (height > result.height) {
+                    result.height = height;
+                }
+                if (index != 1) {
+                    result.width += drawable.getMinimumWidth();
+                } else {
+                    result.width += Math.max(drawable.getMinimumWidth(), textWidth);
                 }
             }
         }
-        Paint.FontMetricsInt fontMetricsInt = mTextPaint.getFontMetricsInt();
-        return Math.max(height, fontMetricsInt.bottom - fontMetricsInt.top);
-    }
-
-    private void fixBounds() {
-        final int viewWidth = getWidth();
-        if (mDrawables[0].getMinimumWidth() == 0
-                && mDrawables[2].getMinimumWidth() == 0
-                && mDrawables[1].getMinimumWidth() == 0) {
-            int width = viewWidth / 3;
-            mBounds[0].left = 0;
-            mBounds[0].right = mBounds[0].left + width;
-            mBounds[2].right = viewWidth;
-            mBounds[2].left = mBounds[2].right - width;
-            mBounds[1].left = mBounds[0].right;
-            mBounds[1].right = mBounds[2].left;
-        }
+        int textHeightWithPadding = textHeight + paddingTop + paddingBottom;
+        int textWidthWithPadding = textWidth + paddingStart + paddingEnd;
+        result.width = Math.max(result.width, textWidthWithPadding);
+        result.height = Math.max(result.height, textHeightWithPadding);
+        return result;
     }
 
     private PointF computeTextStart() {
@@ -168,4 +257,8 @@ public class TernaryTextView extends View {
         return new PointF((getWidth() - width) / 2, y);
     }
 
+    private static class Size {
+        private int width = 0;
+        private int height = 0;
+    }
 }
