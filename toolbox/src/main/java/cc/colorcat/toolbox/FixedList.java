49ee61b229c1bc7313417670ea768bbcf9ee0377
cxx <xx.ch@outlook.com>
2017-09-07 23:02:22 +0800

    add FixedList

---
+++ toolbox/src/main/java/cc/colorcat/toolbox/FixedList.java
@@ -0,0 +1,53 @@
+package cc.colorcat.toolbox;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Created by cxx on 2017/9/7.
+ * xx.ch@outlook.com
+ */
+public class FixedList<T> extends ArrayList<T> {
+    private final int maxSize;
+
+    public FixedList(int maxSize) {
+        super(maxSize + 1);
+        if (maxSize < 1) {
+            throw new IllegalArgumentException("maxSize < 1");
+        }
+        this.maxSize = maxSize;
+    }
+
+    @Override
+    public boolean add(T t) {
+        boolean result = super.add(t);
+        fixSize();
+        return result;
+    }
+
+    @Override
+    public void add(int index, T element) {
+        super.add(index, element);
+        fixSize();
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        boolean result = super.addAll(c);
+        fixSize();
+        return result;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        boolean result = super.addAll(index, c);
+        fixSize();
+        return result;
+    }
+
+    private void fixSize() {
+        while (size() > maxSize) {
+            remove(0);
+        }
+    }
+}
