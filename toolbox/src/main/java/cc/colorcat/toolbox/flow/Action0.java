f3621933fd98496bef28294433985881f7ce68be
cxx <xx.ch@outlook.com>
2017-09-25 09:40:48 +0800

    add Flow

--- toolbox/src/main/java/cc/colorcat/toolbox/flow/Action0.java
+++ toolbox/src/main/java/cc/colorcat/toolbox/flow/Action0.java
@@ -1,6 +1,5 @@
 package cc.colorcat.toolbox.flow;
 
 public interface Action0 extends Action {
-
     void call();
 }
--- toolbox/src/main/java/cc/colorcat/toolbox/flow/Action1.java
+++ toolbox/src/main/java/cc/colorcat/toolbox/flow/Action1.java
@@ -1,6 +1,5 @@
 package cc.colorcat.toolbox.flow;
 
 public interface Action1<T> extends Action {
-
     void call(T t);
 }
