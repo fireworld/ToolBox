5f634f1b3ae75b4dd0299699322d3b218fc2a3b2
cxx <xx.ch@outlook.com>
2017-10-30 10:07:35 +0800

    mod RvAdapter

--- toolbox/src/main/java/cc/colorcat/toolbox/widget/RvOnLoadMoreScrollListener.java
+++ toolbox/src/main/java/cc/colorcat/toolbox/widget/RvOnLoadMoreScrollListener.java
@@ -17,22 +17,18 @@ public abstract class RvOnLoadMoreScrollListener extends RecyclerView.OnScrollLi
     @Override
     public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
         super.onScrollStateChanged(recyclerView, newState);
-//        L.i("onScrollStateChanged, newState = " + newState);
         RecyclerView.LayoutManager manager = recyclerView.getLayoutManager();
         int total = manager.getItemCount();
         View view = manager.findViewByPosition(total - 1);
         if (mUpOnLast && view != null && newState == RecyclerView.SCROLL_STATE_IDLE) {
-//            L.e("you need more");
             onLoadMore();
         }
-//        L.d("total = " + total + ", the last view + " + view);
     }
 
     @Override
     public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
         super.onScrolled(recyclerView, dx, dy);
         mUpOnLast = dy > 0;
-//        L.e("dx = " + dx + ", dy = " + dy + ", mUpOnLast = " + mUpOnLast);
     }
 
     public abstract void onLoadMore();
