ebc7f1f865352919d6f47358146f19706583e413
cxx <xx.ch@outlook.com>
2017-11-28 11:20:58 +0800

    1. add ChoiceRvAdapter and SimpleChoiceRvAdapter

--- toolbox/src/main/java/cc/colorcat/toolbox/widget/RvAdapter.java
+++ toolbox/src/main/java/cc/colorcat/toolbox/widget/RvAdapter.java
@@ -5,16 +5,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 
 /**
  * Created by cxx on 2017/8/13.
  * xx.ch@outlook.com
  */
 public abstract class RvAdapter extends RecyclerView.Adapter<RvHolder> {
-    private int mSelectedPosition = AdapterView.INVALID_POSITION;
-    private OnItemSelectedListener mSelectedListener;
-
     @Override
     public final RvHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(getLayoutResId(viewType), parent, false);
@@ -24,45 +20,13 @@ public abstract class RvAdapter extends RecyclerView.Adapter<RvHolder> {
     }
 
     @Override
-    public final void onBindViewHolder(RvHolder holder, int position) {
-        RvHolder.Helper helper = holder.getHelper();
-        helper.setViewType(holder.getItemViewType()).setPosition(position);
-        helper.getRoot().setSelected(mSelectedPosition == position);
+    public void onBindViewHolder(RvHolder holder, int position) {
+        holder.getHelper().setViewType(holder.getItemViewType()).setPosition(position);
         bindView(holder, position);
-    }
-
-    public void setOnItemSelectedListener(OnItemSelectedListener listener) {
-        mSelectedListener = listener;
-    }
-
-    public OnItemSelectedListener getOnItemSelectedListener() {
-        return mSelectedListener;
-    }
-
-    public void setSelection(int position) {
-        if (mSelectedPosition != position && position >= 0 && position < getItemCount()) {
-            int last = mSelectedPosition;
-            mSelectedPosition = position;
-            notifyItemChanged(last);
-            notifyItemChanged(mSelectedPosition);
-            if (mSelectedListener != null) {
-                mSelectedListener.onItemSelected(position);
-            }
-        }
-    }
-
-    public int getSelection() {
-        return mSelectedPosition;
     }
 
     @LayoutRes
     public abstract int getLayoutResId(int viewType);
 
     public abstract void bindView(RvHolder holder, int position);
-
-
-    public interface OnItemSelectedListener {
-
-        void onItemSelected(int position);
-    }
 }
